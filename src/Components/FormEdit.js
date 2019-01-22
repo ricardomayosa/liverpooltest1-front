3493ce01fc11818d60c8e3b5a10ec2a3ab081bc7
Ricardo Mayo <rmayo.1995@gmail.com>
2019-01-22 15:08:32 -0600

    corrected hosting path

--- src/Components/FormEdit.js
+++ src/Components/FormEdit.js
@@ -17,7 +17,7 @@ export default class FormEdit extends Component {
 		const baseURL =
 			window.location.hostname === 'localhost'
 				? 'http://localhost:3000/article'
-				: 'https://liverpooltest-1.herokuapp.com';
+				: 'https://liverpooltest-1.herokuapp.com/article';
 		axios
 			.get(`${baseURL}/${this.props.match.params.id}`)
 			.then(res => {
